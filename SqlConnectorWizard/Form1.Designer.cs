3acf1edd24f2266f8530f63822f3c4eacbf8776f
elvis <elviskcheruiyot@gmail.com>
2020-08-27 14:41:37 +0300

    updated sql connector

---
+++ SqlConnectorWizard/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace SqlConnectorWizard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEazzyUrl = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.TillNo = new System.Windows.Forms.Label();
+            this.txtEazzyTillNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.dbCombobox = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtmpesaUrl = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMpesaTillNo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtEazzyUrl
+            // 
+            this.txtEazzyUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEazzyUrl.Location = new System.Drawing.Point(176, 186);
+            this.txtEazzyUrl.Name = "txtEazzyUrl";
+            this.txtEazzyUrl.Size = new System.Drawing.Size(525, 26);
+            this.txtEazzyUrl.TabIndex = 19;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(372, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(184, 43);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TillNo
+            // 
+            this.TillNo.AutoSize = true;
+            this.TillNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TillNo.Location = new System.Drawing.Point(6, 157);
+            this.TillNo.Name = "TillNo";
+            this.TillNo.Size = new System.Drawing.Size(124, 20);
+            this.TillNo.TabIndex = 16;
+            this.TillNo.Text = "EazzyPay Till No";
+            // 
+            // txtEazzyTillNo
+            // 
+            this.txtEazzyTillNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEazzyTillNo.Location = new System.Drawing.Point(176, 157);
+            this.txtEazzyTillNo.Name = "txtEazzyTillNo";
+            this.txtEazzyTillNo.Size = new System.Drawing.Size(195, 26);
+            this.txtEazzyTillNo.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "EazzyPay URL";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServerName.Location = new System.Drawing.Point(176, 12);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(321, 26);
+            this.txtServerName.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Server Name ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "User Name ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Password ";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(176, 44);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(321, 26);
+            this.txtUserName.TabIndex = 24;
+            // 
+            // dbCombobox
+            // 
+            this.dbCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dbCombobox.FormattingEnabled = true;
+            this.dbCombobox.Location = new System.Drawing.Point(176, 106);
+            this.dbCombobox.Name = "dbCombobox";
+            this.dbCombobox.Size = new System.Drawing.Size(321, 28);
+            this.dbCombobox.TabIndex = 28;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(176, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(321, 26);
+            this.txtPassword.TabIndex = 25;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(500, 71);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(180, 28);
+            this.btnCheck.TabIndex = 26;
+            this.btnCheck.Text = "Check Connection";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 20);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Select Database";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(582, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 43);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtmpesaUrl
+            // 
+            this.txtmpesaUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmpesaUrl.Location = new System.Drawing.Point(176, 264);
+            this.txtmpesaUrl.Name = "txtmpesaUrl";
+            this.txtmpesaUrl.Size = new System.Drawing.Size(525, 26);
+            this.txtmpesaUrl.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 20);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "M-PESA Till No";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtMpesaTillNo
+            // 
+            this.txtMpesaTillNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMpesaTillNo.Location = new System.Drawing.Point(176, 233);
+            this.txtMpesaTillNo.Name = "txtMpesaTillNo";
+            this.txtMpesaTillNo.Size = new System.Drawing.Size(195, 26);
+            this.txtMpesaTillNo.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "M-PESA URL";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCheck);
+            this.panel1.Location = new System.Drawing.Point(3, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(712, 143);
+            this.panel1.TabIndex = 34;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 154);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(711, 64);
+            this.panel2.TabIndex = 35;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 224);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(710, 77);
+            this.panel3.TabIndex = 36;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SqlConnectorWizard.Properties.Resources.United_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 315);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 378);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtmpesaUrl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMpesaTillNo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtServerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.dbCombobox);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEazzyUrl);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.TillNo);
+            this.Controls.Add(this.txtEazzyTillNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(404, 312);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client Configuration ..";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEazzyUrl;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label TillNo;
+        private System.Windows.Forms.TextBox txtEazzyTillNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.ComboBox dbCombobox;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtmpesaUrl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMpesaTillNo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SqlConnectorWizard/Form1.cs
@@ -0,0 +1,222 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SqlConnectorWizard
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String Servername = txtServerName.Text;
+               
+                String UserName = txtUserName.Text;
+                String Password = txtPassword.Text;
+                String connetionString = "Data Source=" + Servername + "; User ID = " + UserName + "; Password = " + Password;
+                SqlConnection cnn;
+                cnn = new SqlConnection(connetionString);
+
+               // cnn.Open();
+                
+                List <string> Dbs = GetDatabaseList(cnn);
+                dbCombobox.DataSource = Dbs;
+               // cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public List<string> GetDatabaseList(SqlConnection con)
+        {
+            List<string> list = new List<string>();
+            try
+            {           
+
+                // Open connection to the database
+
+
+                con.Open();
+                MessageBox.Show("Connection Established");
+                // Set up a command with the given query and associate
+                // this with the current connection.
+                using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con))
+                {
+                    using (IDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            list.Add(dr[0].ToString());
+                        }
+                    }
+                }
+                con.Close();
+
+                return list;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return list;
+            }
+        }
+        public static void EditconnetionString(String URL)
+        {
+            try
+            {
+                String iniPath = AppDomain.CurrentDomain.BaseDirectory+ "connetionString.ini";            
+                StringBuilder newFile = new StringBuilder();                
+                newFile.Append(URL);
+                File.WriteAllText(@iniPath, newFile.ToString());
+            }
+            catch (Exception)
+            {
+                //  log.Error(e);
+
+            }
+
+        }
+        public static void EditTillFile(String param,String desc)
+        {
+            try
+            {
+                if (desc == "MpesaTill")
+                {
+                    String iniPath = AppDomain.CurrentDomain.BaseDirectory + "MpesaTill.ini";
+                    StringBuilder newFile = new StringBuilder();
+                    newFile.AppendLine(param);
+                    File.WriteAllText(@iniPath, newFile.ToString());
+                }else if (desc == "MpesaUrl")
+                {
+                    String iniPath = AppDomain.CurrentDomain.BaseDirectory + "MpesaUrl.ini";
+                    StringBuilder newFile = new StringBuilder();
+                    newFile.AppendLine(param);
+                    File.WriteAllText(@iniPath, newFile.ToString());
+                }
+                else if (desc == "EazzyTill")
+                {
+                    String iniPath = AppDomain.CurrentDomain.BaseDirectory + "EazzyTill.ini";
+                    StringBuilder newFile = new StringBuilder();
+                    newFile.AppendLine(param);
+                    File.WriteAllText(@iniPath, newFile.ToString());
+                }
+                else if (desc == "EazzyUrl")
+                {
+                    String iniPath = AppDomain.CurrentDomain.BaseDirectory + "EazzyUrl.ini";
+                    StringBuilder newFile = new StringBuilder();
+                    newFile.AppendLine(param);
+                    File.WriteAllText(@iniPath, newFile.ToString());
+                }
+                else
+                {
+
+                }
+
+            }
+            catch (Exception)
+            {
+                //  log.Error(e);
+
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if ( String.IsNullOrEmpty(txtUserName.Text)
+                 || String.IsNullOrEmpty(txtServerName.Text)
+                 || String.IsNullOrWhiteSpace(dbCombobox.SelectedItem.ToString()))
+                {
+                    MessageBox.Show("Fill All Database  Fields are Required");
+                    return;
+                }
+               
+                String Servername = txtServerName.Text;
+                String DatabaseName = dbCombobox.SelectedItem.ToString();
+                String UserName = txtUserName.Text;
+                String Password = txtPassword.Text;
+
+                String connetionString = "Data Source=" + Servername + ";Initial Catalog = " + DatabaseName + "; User ID = " + UserName + "; Password = " + Password;
+                String en=Crypto.Encrypt(connetionString);
+
+                String EazzyTillNo = txtEazzyTillNo.Text;
+                String EazzyUrl = txtEazzyUrl.Text;
+
+                String MpesaTillNo = txtMpesaTillNo.Text;
+                String mpesaUrl = txtmpesaUrl.Text;
+               
+
+                if (!String.IsNullOrEmpty(EazzyTillNo) && !String.IsNullOrEmpty(EazzyUrl))
+                {
+                    //save eazzy url and till
+                    String enEazzyTillNo = Crypto.Encrypt(EazzyTillNo);
+                    String enEazzyUrl = Crypto.Encrypt(EazzyUrl);
+                    EditTillFile(enEazzyTillNo, "EazzyTill");
+                    EditTillFile(enEazzyUrl, "EazzyUrl");
+                }
+                if (!String.IsNullOrEmpty(MpesaTillNo) && !String.IsNullOrEmpty(mpesaUrl))
+                {
+                    //save mpesa url adn till
+                    String enMpesaTillNo = Crypto.Encrypt(MpesaTillNo);
+                    String enmpesaUrl = Crypto.Encrypt(mpesaUrl);
+                    EditTillFile(enMpesaTillNo, "MpesaTill");
+                    EditTillFile(enmpesaUrl, "MpesaUrl");
+                }
+                EditconnetionString(en);
+               
+                // String conn = Crypto.Decrypt(en);
+                MessageBox.Show("Configurations Saved");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            txtPassword.PasswordChar = '*';
+            // log.Info("Hello logging world!");
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
